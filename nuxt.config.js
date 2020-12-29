778de2b5e7907dc09f238b654325d44c77414e79
jimpei <j.hamamura0621@gmail.com>
2020-12-30 01:21:10 +0900

    deploy firebase hosting

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,7 @@
+const myCityNameJp = process.env.MY_CITY_NAME_JP || '名前無し'
+const myCityNameEn = process.env.MY_CITY_NAME_EN || 'NoName'
+const environment = 'prod'
+
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
@@ -7,7 +11,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'kadai-map',
+    title: myCityNameEn + '-kadai-map',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -36,6 +40,7 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
+    ['@nuxtjs/dotenv', { filename: `.env.${environment}` }],
   ],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
---
+++ plugins/firebase.js
@@ -0,0 +1,24 @@
+import firebase from '@firebase/app'
+import '@firebase/auth'
+import '@firebase/firestore'
+// import '@firebase/functions'
+import '@firebase/storage'
+// import '@firebase/analytics'
+
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  // measurementId: process.env.MEASUREMENT_ID
+}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+  firebase.analytics();
+}
+
+export default firebase
