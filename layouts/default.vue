bcdd4a8b853997894df9f4d6070beb13d35b5f08
jimpei <j.hamamura0621@gmail.com>
2020-12-30 22:12:44 +0900

    layoutをわかりやすく

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,10 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
+    <!-- sidebar   -->
+    <v-navigation-drawer v-model="drawer" :clipped="clipped" fixed app>
+      <div class="side-title">{{title}}</div>
       <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
+        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -25,46 +15,35 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <!-- header   -->
+    <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-<!--      <v-btn-->
-<!--        icon-->
-<!--        @click.stop="miniVariant = !miniVariant"-->
-<!--      >-->
-<!--        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>-->
-<!--      </v-btn>-->
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
+      <v-btn icon @click.stop="clipped = !clipped">
         <v-icon>mdi-application</v-icon>
       </v-btn>
-<!--      <v-btn-->
-<!--        icon-->
-<!--        @click.stop="fixed = !fixed"-->
-<!--      >-->
-<!--        <v-icon>mdi-minus</v-icon>-->
-<!--      </v-btn>-->
       <v-toolbar-title v-text="title" />
       <v-spacer />
     </v-app-bar>
+
+    <!-- main   -->
     <v-main>
       <v-container>
         <nuxt />
       </v-container>
     </v-main>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
+
+    <!-- footer   -->
+    <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
+
+<style scoped>
+  .side-title {
+    margin: 10px;
+  }
+</style>
 
 <script>
 export default {
@@ -87,7 +66,7 @@ export default {
           to: '/inspire'
         }
       ],
-      miniVariant: false,
+      // miniVariant: false,
       right: true,
       rightDrawer: false,
     }
