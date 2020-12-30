c6000e0a00c7c0e31596eb17099a86741f27102c
jimpei <j.hamamura0621@gmail.com>
2020-12-31 00:44:09 +0900

    modal作成

---
+++ components/ModalAddData.vue
@@ -0,0 +1,99 @@
+<template>
+  <div id="app">
+    <v-dialog v-model="dialog" max-width="700px">
+
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn color="primary" dark v-bind="attrs" v-on="on">
+          課題を追加
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title>
+          <span class="headline">課題を追加</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field label="聞き取り日" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field label="題目" hint="example of helper text only on focus"></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  label="分類"
+                  hint="example of persistent helper text"
+                  persistent-hint
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field label="内容" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field label="対応案" type="password" required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-select
+                  :items="['0-17', '18-29', '30-54', '54+']"
+                  label="情報源"
+                  required
+                ></v-select>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-autocomplete
+                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                  label="地区"
+                  multiple
+                ></v-autocomplete>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-autocomplete
+                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                  label="取り組みステータス"
+                  multiple
+                ></v-autocomplete>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field label="関係人口" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field label="緊急度" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field label="実現難易度" required></v-text-field>
+              </v-col>
+
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog = false">
+            Close
+          </v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {
+    },
+    data() {
+      return {
+        dialog: false,
+      }
+    }
+  }
+</script>
---
+++ components/ModalLogin.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <v-dialog v-model="dialog" max-width="700px">
+
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn color="primary" dark v-bind="attrs" v-on="on">
+          ログイン
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title>
+          <span class="headline">ログイン</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="12" md="6">
+                <v-text-field outlined dense label="メールアドレス" type="email" hint=""></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="12" md="6">
+                <v-text-field
+                  outlined
+                  dense
+                  :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                  :type="showPassword ? 'text' : 'password'"
+                  name="input-10-2"
+                  label="パスワード"
+                  hint="At least 6 characters"
+                  @click:append="showPassword = !showPassword"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog = false">
+            Close
+          </v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {
+    },
+    data() {
+      return {
+        dialog: false,
+        showPassword: false,
+        rules: {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 8 || 'Min 8 characters',
+        },
+      }
+    }
+  }
+</script>
