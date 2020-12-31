79870b1aaa29c21eea8c8e5b1a18e5a773e07480
jimpei <j.hamamura0621@gmail.com>
2020-12-31 16:26:13 +0900

    バブルチャートのベースを作成

--- components/BarChart.vue
+++ components/BarChart.vue
@@ -1,26 +1,24 @@
 <template>
-  <div class="w-full xl:w-4/12 px-4">
-<!--    {{issueListSize}}-->
-<!--    {{issueList}}-->
-    <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
-      <div class="rounded-t mb-0 px-4 py-3 bg-transparent">
-        <div class="flex flex-wrap items-center">
-          <div class="relative w-full max-w-full flex-grow flex-1">
-            <h6 class="uppercase text-gray-500 mb-1 text-xs font-semibold">
-              Performance
-            </h6>
-            <h2 class="text-gray-800 text-xl font-semibold">
-              Total orders
-            </h2>
+  <div>
+    <div class="w-full xl:w-4/12 px-4">
+    <!--    {{issueListSize}}-->
+    <!--    {{issueList}}-->
+      <v-sheet elevation="2" class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+        <div class="rounded-t mb-0 px-4 py-3 bg-transparent">
+          <div class="flex flex-wrap items-center">
+            <div class="relative w-full max-w-full flex-grow flex-1">
+              <h6 class="uppercase text-gray-500 mb-1 text-xs font-semibold">Performance</h6>
+              <h2 class="text-gray-800 text-xl font-semibold">Total orders</h2>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="p-4 flex-auto">
-        <!-- Chart -->
-        <div class="relative" style="height:350px">
-          <canvas id="bar-chart"></canvas>
+        <div class="p-4 flex-auto">
+          <!-- Chart -->
+          <div class="relative" style="height:350px">
+            <canvas id="bar-chart"></canvas>
+          </div>
         </div>
-      </div>
+      </v-sheet>
     </div>
   </div>
 </template>
@@ -40,46 +38,51 @@ export default {
     this.selectDB();
     this.$nextTick(function() {
       let config = {
-        type: "bar",
+        type: "bubble",
         data: {
-          labels: [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July"
-          ],
           datasets: [
             {
-              label: new Date().getFullYear(),
-              backgroundColor: "#ed64a6",
-              borderColor: "#ed64a6",
-              data: [30, 78, 56, 34, 100, 45, 13],
-              fill: false,
-              barThickness: 8
+              data: [{"x":10 ,"y":10, "r":10} ,],
+              backgroundColor:[ "rgb(141,63,223)" ],
+              label: ["test1"]
             },
             {
-              label: new Date().getFullYear() - 1,
-              fill: false,
-              backgroundColor: "#4c51bf",
-              borderColor: "#4c51bf",
-              data: [27, 68, 86, 74, 10, 4, 87],
-              barThickness: 8
+              data: [{"x":-20 ,"y":-20, "r":20} ,],
+              backgroundColor:[ "rgb(141,29,73)"],
+              label: ["test2"]
+            },
+            {
+              data: [{"x":30 ,"y":30, "r":30} ,],
+              backgroundColor:["rgb(16,230,73)"],
+              label: ["test3"]
             }
           ]
         },
         options: {
-          maintainAspectRatio: false,
-          responsive: true,
           title: {
             display: false,
             text: "Orders Chart"
           },
+          scales: {
+            // x軸
+            xAxes: [{
+              ticks: {max: 50, min: -50,stepSize: 10}
+            }],
+            // x軸
+            yAxes: [{
+              ticks: {max: 50,min: -50,stepSize: 10}
+            }]
+          },
+          maintainAspectRatio: false,
+          responsive: true,
           tooltips: {
-            mode: "index",
-            intersect: false
+            callbacks: {
+              label: function(t, d) {
+                var rLabel = d.datasets[t.datasetIndex].data[t.index].r;
+                return d.datasets[t.datasetIndex].label +
+                  ': (x軸:' + t.xLabel + ', y軸:' + t.yLabel + ', 円の大きさ:' + rLabel + ')';
+              }
+            }
           },
           hover: {
             mode: "nearest",
@@ -92,43 +95,6 @@ export default {
             align: "end",
             position: "bottom"
           },
-          scales: {
-            xAxes: [
-              {
-                display: false,
-                scaleLabel: {
-                display: true,
-                labelString: "Month"
-                },
-                gridLines: {
-                  borderDash: [2],
-                  borderDashOffset: [2],
-                  color: "rgba(33, 37, 41, 0.3)",
-                  zeroLineColor: "rgba(33, 37, 41, 0.3)",
-                  zeroLineBorderDash: [2],
-                  zeroLineBorderDashOffset: [2]
-                }
-              }
-            ],
-            yAxes: [
-              {
-                display: true,
-                scaleLabel: {
-                display: false,
-                labelString: "Value"
-                },
-                gridLines: {
-                  borderDash: [2],
-                  drawBorder: false,
-                  borderDashOffset: [2],
-                  color: "rgba(33, 37, 41, 0.2)",
-                  zeroLineColor: "rgba(33, 37, 41, 0.15)",
-                  zeroLineBorderDash: [2],
-                  zeroLineBorderDashOffset: [2]
-                }
-              }
-            ]
-          }
         }
       };
       let ctx = document.getElementById("bar-chart").getContext("2d");
